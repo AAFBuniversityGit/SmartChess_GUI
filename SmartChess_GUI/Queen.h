434ba51017461e390de4a2b53a73f9d072fd5e40
Amirali <106759325+AAFBuniversityGit@users.noreply.github.com>
2022-06-05 13:50:38 +0430

    class Queen defineded

--- SmartChess_GUI/Queen.h
+++ SmartChess_GUI/Queen.h
@@ -1,1 +1,15 @@
 #pragma once
+#include "Piece.h"
+
+class Queen : public Piece
+{
+public:
+	Queen(char);
+
+	~Queen();
+
+private:
+	virtual char GetPiece();
+
+	bool AllowedCoordinates(int sourceRow, int sourceColumn, int destinationRow, int destinationColumn, Piece* Board[8][8]);
+};
