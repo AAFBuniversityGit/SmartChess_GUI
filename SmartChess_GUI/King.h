070133607bd12093783b249c5594480bfcc6d080
Amirali <106759325+AAFBuniversityGit@users.noreply.github.com>
2022-06-05 13:49:41 +0430

    class King defineded

--- SmartChess_GUI/King.h
+++ SmartChess_GUI/King.h
@@ -1,1 +1,15 @@
 #pragma once
+#include "Piece.h"
+
+class King : public Piece
+{
+public:
+	King(char);
+
+	~King();
+
+private:
+	virtual char GetPiece();
+
+	bool AllowedCoordinates(int sourceRow, int sourceColumn, int destinationRow, int destinationColumn, Piece* Board[8][8]);
+};
