0fae7730e907f7bededa09d7cd95c0c232bfeb16
Amirali <106759325+AAFBuniversityGit@users.noreply.github.com>
2022-06-05 13:53:00 +0430

    class Pawn defineded

--- SmartChess_GUI/Pawn.h
+++ SmartChess_GUI/Pawn.h
@@ -1,1 +1,14 @@
 #pragma once
+#include "Piece.h"
+
+class Pawn : public Piece
+{
+public:
+	Pawn(char);
+	~Pawn();
+
+private:
+	virtual char GetPiece();
+
+	bool AllowedCoordinates(int sourceRow, int sourceColumn, int destinationRow, int destinationColumn, Piece* Board[8][8]);
+};
