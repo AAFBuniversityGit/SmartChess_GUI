20041de267f7fe245ca69e43fcd955f5e2b9514c
Amirali <106759325+AAFBuniversityGit@users.noreply.github.com>
2022-06-05 13:48:13 +0430

    class Piece defineded

--- SmartChess_GUI/Piece.h
+++ SmartChess_GUI/Piece.h
@@ -1,1 +1,19 @@
 #pragma once
+
+class Piece
+{
+public:
+	Piece(char);
+
+	~Piece();
+
+	virtual char GetPiece() = 0;
+
+	char GetColor();
+
+	bool Allowed(int sourceRow, int sourceColumn, int destinationRow, int destinationColumn, Piece* Board[8][8]);
+
+private:
+	virtual bool AllowedCoordinates(int sourceRow, int sourceColumn, int destinationRow, int destinationColumn, Piece* [8][8]) = 0;
+	char xPieceColor;
+};
