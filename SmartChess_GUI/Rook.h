698c627ac8e3bd508cf4c1107cdccd04cc9ccea8
Amirali <106759325+AAFBuniversityGit@users.noreply.github.com>
2022-06-05 13:52:34 +0430

    class Rook defineded

--- SmartChess_GUI/Rook.h
+++ SmartChess_GUI/Rook.h
@@ -1,1 +1,15 @@
 #pragma once
+#include "Piece.h"
+
+class Rook : public Piece
+{
+public:
+	Rook(char);
+
+	~Rook();
+
+private:
+	virtual char GetPiece();
+
+	bool AllowedCoordinates(int sourceRow, int sourceColumn, int destinationRow, int destinationColumn, Piece* Board[8][8]);
+};
