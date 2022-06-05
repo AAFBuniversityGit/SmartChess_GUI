209f1984c59d575069d7e3151e079383e2e0388e
Amirali <106759325+AAFBuniversityGit@users.noreply.github.com>
2022-06-05 19:39:31 +0430

    sfml dlls x32 Deleted, sfml dlls x64 Added, main.cpp Changed

--- SmartChess_GUI/main.cpp
+++ SmartChess_GUI/main.cpp
@@ -1,8 +1,24 @@
 #include "Controller.h";
+#include <SFML/Graphics.hpp>
+using namespace sf;
 
 int main()
 {
-    Controller Game;
-    Game.Start();
+    /*Controller Game;
+    Game.Start();*/
+    RenderWindow window(VideoMode(800, 600), "Smart Chess", Style::Titlebar | Style::Close);
+    sf::Event event;
+
+    while (window.isOpen())
+    {
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+    }
+
     return 0;
 }
