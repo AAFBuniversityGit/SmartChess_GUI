99258143086f576ae4954540803cd06f50d83352
Amirali <106759325+AAFBuniversityGit@users.noreply.github.com>
2022-06-05 13:51:22 +0430

    class Bishop defineded

--- SmartChess_GUI/Bishop.h
+++ SmartChess_GUI/Bishop.h
@@ -1,1 +1,15 @@
 #pragma once
+#include "Piece.h"
+
+class Bishop : public Piece
+{
+public:
+	Bishop(char);
+
+	~Bishop();
+
+private:
+	virtual char GetPiece();
+
+	bool AllowedCoordinates(int sourceRow, int sourceColumn, int destinationRow, int destinationColumn, Piece* Board[8][8]);
+};
