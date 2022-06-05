d61f77977fcd9069dbee72df117dca2f7d3c67b8
Amirali <106759325+AAFBuniversityGit@users.noreply.github.com>
2022-06-05 13:52:12 +0430

    class Knight defineded

--- SmartChess_GUI/Knight.h
+++ SmartChess_GUI/Knight.h
@@ -1,1 +1,15 @@
 #pragma once
+#include "Piece.h"
+
+class Knight : public Piece
+{
+public:
+	Knight(char);
+
+	~Knight();
+
+private:
+	virtual char GetPiece();
+
+	bool AllowedCoordinates(int sourceRow, int sourceColumn, int destinationRow, int destinationColumn, Piece* Board[8][8]);
+};
