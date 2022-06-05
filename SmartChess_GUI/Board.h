b8a7199f1c03ceb04af94d213bdbb7ac8260919c
Amirali <106759325+AAFBuniversityGit@users.noreply.github.com>
2022-06-05 13:55:29 +0430

    Board.h Header Added. class Board initialzied

---
+++ SmartChess_GUI/Board.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Piece.h"
+
+class Board
+{
+public:
+	Board();
+	~Board();
+
+	void Print();
+
+	bool IsCheck(char PieceColor);
+
+	bool AllowedToMove(char PieceColor);
+
+	Piece* GameBoard[8][8];
+};
