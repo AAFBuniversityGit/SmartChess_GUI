bd6dd4b1bdaec37c471c287c9fdb3c2de8563e6e
Amirali <106759325+AAFBuniversityGit@users.noreply.github.com>
2022-06-06 11:23:12 +0430

    Setting header and body defineded.

---
+++ SmartChess_GUI/Setting.cpp
@@ -0,0 +1,6 @@
+#include "setting.h"
+
+int setting::CellSize = 100;
+int setting::CellOffset = 20;
+sf::Color setting::CellColor = sf::Color::Black;
+int setting::LineSpace = 20;
---
+++ SmartChess_GUI/Setting.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "SFML/Graphics.hpp"
+
+struct setting
+{
+    static int CellSize;
+    static int CellOffset;
+    static sf::Color CellColor;
+    static int LineSpace;
+};
